aa704f23f6f2f756b35ef3ad02208d1f89b8c264
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2024-10-14 22:15:41 -0300

    update

---
+++ dados_csharp/dados_csharp/Models/Curso.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados_csharp.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno (Pessoa aluno)
+        {
+            Alunos.Add (aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            Console.WriteLine($"Estão inscritos nesse curso {quantidade} alunos: ");
+            return quantidade;
+        }
+
+        public bool RemoverAluno (Pessoa aluno)
+        {
+            return Alunos.Remove (aluno); 
+        }
+
+        public void ListarAlunos()
+        {
+
+            Console.WriteLine($"Alunos do curso de {Nome}");
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.NomeCompleto);
+            }
+        }
+    }
+}
--- dados_csharp/dados_csharp/Program.cs
+++ dados_csharp/dados_csharp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dados_csharp.Models;
+
+Pessoa p1 = new Pessoa("Eduardo", "Vernizzi");
+Pessoa p2 = new Pessoa("Thiago", "Vernizzi");
+
+
+Curso ingles = new Curso();
+
+ingles.Nome = "Inglês";
+ingles.Alunos = new List<Pessoa>();
+ingles.AdicionarAluno(p1);
+ingles.AdicionarAluno(p2);
+ingles.ObterQuantidadeDeAlunosMatriculados();
+ingles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+//Implementação e teste de Métodos
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Eduardo";
+//p1.Sobrenome= "Vernizzi";
+//p1.Idade = 25;
+//p1.Apresentar();
